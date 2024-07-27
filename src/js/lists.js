15d3080e96d47258b836ce1245cc987abc1f82dc
Mani Afschar Yazdi <mani.afschar@jq-consulting.de>
2024-07-27 16:44:26 +0200

    build

--- src/js/lists.js
+++ src/js/lists.js
@@ -36,7 +36,7 @@ class lists {
 		e.setAttribute('class', 'buttons');
 		var s = '', sqls =
 			[
-				{ label: 'log', sql: 'log.createdAt>{date-12h} and log.uri not like \'/support/%\' and log.uri not like \'/marketing/%\'' },
+				{ label: 'log', sql: 'log.createdAt>{date-12h} and log.uri not like \'/support/%\' and log.uri not like \'/marketing/%\' and lower(ip.org) not like \'%facebook%\' and lower(ip.org) not like \'%google%\'' },
 				{ label: 'support', sql: 'log.createdAt>{date-3h} and log.uri like \'/support/%\'' },
 				{ label: 'error', sql: 'ticket.type=\'ERROR\'' },
 				{ label: 'ad', sql: 'log.createdAt>{date-1} and log.uri not like \'/%\'' },
